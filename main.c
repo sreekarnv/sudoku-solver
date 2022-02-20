3b468d44af2109e0e5a685c7b0c32d7baf95bb4d
Sai Ankit <saiankit30@gmail.com>
2022-02-21 00:06:38 +0530

    feat(main): add skeleton for the code so as to comply with the given input and output formats
    BREAKING CHANGE:

--- main.c
+++ main.c
@@ -1,29 +1,31 @@
-#include <stdio.h>
+/* Do not make any changes in the skeleton. Your submission will be invalidated if the skeleton is changed */
 
-void displaySudokuGrid(int arr[9][9])
-{
-    for (int i = 0; i < 9; i++)
-    {
-        for (int j = 0; j < 9; j++)
-        {
-            if (j % 3 == 0)
-            {
-                printf("|");
-            }
 
-            printf("%d", arr[i][j]);
+# include <stdio.h>
+# include <stdlib.h>
+
+void read_grid_from_file(int size, char *ip_file, int grid[36][36]) {
+    FILE *fp;
+    int i, j;
+    fp = fopen(ip_file, "r");
+    for (i = 0; i < size; i++)
+        for (j = 0; j < size; j++) {
+            fscanf(fp, "%d", &grid[i][j]);
         }
+}
 
+void print_grid(int size, int grid[36][36]) {
+    int i, j;
+    /* The segment below prints the grid in a standard format. Do not change */
+    for (i = 0; i < size; i++) {
+        for (j = 0; j < size; j++)
+            printf("%d\t", grid[i][j]);
         printf("\n");
-
-        if (i != 0 && (i + 1) % 3 == 0)
-        {
-            printf("---------------------------------\n");
-        }
     }
 }
 
-int isPositionValid(int grid[9][9], int y, int x, int n)
+
+int isPositionValid(int grid[36][36], int y, int x, int n)
 {
 
     for (int i = 0; i <= 8; i++)
@@ -59,7 +61,7 @@ int isPositionValid(int grid[9][9], int y, int x, int n)
     return 1;
 }
 
-int bruteForceSudokuRecurse(int sudoku[9][9], int y, int x)
+int bruteForceSudokuRecurse(int sudoku[36][36], int y, int x)
 {
     if (y == 9 && x == 8)
     {
@@ -95,26 +97,23 @@ int bruteForceSudokuRecurse(int sudoku[9][9], int y, int x)
     return 0;
 }
 
-int main()
-{
-    int sudoku[9][9] = {
-        {6, 0, 0, 8, 7, 0, 0, 0, 0},
-        {0, 0, 9, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 1, 0, 0, 8, 0, 0},
-        {7, 9, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 1, 0, 8, 0, 0, 0, 0},
-        {0, 0, 0, 4, 1, 0, 0, 0, 0},
-        {0, 7, 0, 0, 5, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0, 7},
-    };
+int main(int argc, char *argv[]) {
+    int grid[36][36], size, i, j;
 
-    if (bruteForceSudokuRecurse(sudoku, 0, 0) == 0)
+    if (argc != 3) {
+        printf("Usage: ./sudoku.out grid_size inputfile");
+        exit(-1);
+    }
+
+    size = atoi(argv[1]);
+    read_grid_from_file(size, argv[2], grid);
+
+    /* Do your thing here */
+    if (bruteForceSudokuRecurse(grid, 0, 0) == 0)
     {
         printf("No solution exists");
     }
 
-    displaySudokuGrid(sudoku);
 
-    return 0;
+    print_grid(size, grid);
 }
